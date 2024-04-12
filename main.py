06a72a9eff43e6b2cc7ea27fed9905f26088b7f1
unknown <unekue2@gmail.com>
2024-04-12 08:08:29 +0100

    added readme.md and setup logger/logging

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+import logging
+import logging.config
 
+# set log level
+# logging.basicConfig(level=logging.DEBUG)
+
+# setup logging
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+
+# app
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"why are you running"}
